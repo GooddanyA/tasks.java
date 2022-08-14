c381913af46ee9cfbc6850c7027d84fb074f202b
Platonov Daniil <platonov.danik@bk.ru>
2022-08-15 01:40:33 +0300

    Intil Commit

--- src/com/company/whileDoWhile/task3.java
+++ src/com/company/whileDoWhile/task3.java
@@ -1,4 +1,22 @@
 package com.company.whileDoWhile;
 
+import java.util.Scanner;
+
+/*Read an integer number N from the input and print all the squares of positive integers:
+less than or equal to N,
+in ascending order.
+*/
 public class task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = 1;
+        while (b <= a) {
+            int f = (int) Math.sqrt(b);
+            if (f * f == b) {
+                System.out.println(b);
+            }
+            b++;
+        }
+    }
 }
