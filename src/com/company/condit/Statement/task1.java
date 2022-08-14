eb7bcdca3124dde97a4615a061a73297301f39d8
Platonov Daniil <platonov.danik@bk.ru>
2022-08-14 23:03:08 +0300

    Intil Commit

---
+++ src/com/company/condit/Statement/task1.java
@@ -0,0 +1,23 @@
+package com.company.condit.Statement;
+
+import java.util.Scanner;
+/* think of chocolate as an N x M rectangle divided into little segments: N segments in length and M in width.
+Each segment is 1x1 and unbreakable.
+Find out whether it is possible to break off exactly K segments from the chocolate with a single straight line: vertical or horizontal.*/
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int k = scanner.nextInt();
+        if (n * m > k) {
+            if (k % n == 0 || k % m == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
