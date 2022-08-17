0582bfe45a654c975ec5f6c836af6432eff53a1a
Platonov Daniil <platonov.danik@bk.ru>
2022-08-17 15:35:47 +0300

    Add

--- src/com/company/whileDoWhile/task5.java
+++ src/com/company/whileDoWhile/task5.java
@@ -1,7 +1,8 @@
 package com.company.whileDoWhile;
 
 import java.util.Scanner;
-
+/*A user inputs a long positive number m.
+You need to find out what is the smallest int number n such that n! > m.*/
 public class task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
